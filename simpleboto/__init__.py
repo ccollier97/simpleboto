ff01f0df4a671b1d44841bbcbd473f0e0abe88ad
charlie_collier97 <charlie.collier@marketcast.com>
2023-09-17 20:01:21 +0100

    remove S3Client reference in __init__

--- simpleboto/__init__.py
+++ simpleboto/__init__.py
@@ -1,6 +1,5 @@
-from simpleboto.s3 import S3Client, S3Url
+from simpleboto.s3 import S3Url
 
 __all__ = [
-    'S3Client',
     'S3Url'
 ]
--- simpleboto/s3/__init__.py
+++ simpleboto/s3/__init__.py
@@ -1,7 +1,5 @@
-from simpleboto.s3.s3_client import S3Client
 from simpleboto.s3.s3_url import S3Url
 
 __all__ = [
-    'S3Client',
     'S3Url'
 ]
